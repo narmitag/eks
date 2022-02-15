607028c12a5696dc9707d0fc0ce468052a027a44
Neil Armitage <neil@neila.co.uk>
2022-02-15 15:43:40 +0000

    updates

--- cluster-autoscaler/setup-eks.sh
+++ cluster-autoscaler/setup-eks.sh
@@ -1,15 +1,9 @@
-sudo curl --silent --location -o /usr/local/bin/kubectl \
-   https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-sudo chmod +x /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 
-sudo yum -y install jq gettext bash-completion moreutils
-
-echo 'yq() {
-  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
-}' | tee -a ~/.bashrc && source ~/.bashrc
-
+sudo yum -y install jq gettext bash-completion moreutils envsubst
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
@@ -20,43 +14,50 @@ eksctl completion bash >> ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
-cat << EOF > eksworkshop.yaml
----
-apiVersion: eksctl.io/v1alpha5
-kind: ClusterConfig
+eksctl create cluster -f ca-cluster.yaml
 
-metadata:
-  name: eksworkshop-eksctl
-  region: us-east-1
-  version: "1.19"
+export CLUSTER_NAME=ca-cluster
 
-availabilityZones: ["us-east-1a", "us-east-1b", "us-east-1c"]
+export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
-managedNodeGroups:
-- name: nodegroupA
-  desiredCapacity: 1
-  availabilityZones: us-east-1a
-  instanceType: t3.small
-  ssh:
-    enableSsm: true
-- name: nodegroupB
-  desiredCapacity: 1
-  availabilityZones: us-east-1b
-  instanceType: t3.small
-  ssh:
-    enableSsm: true
-- name: nodegroupC
-  desiredCapacity: 1
-  availabilityZones: us-east-1c
-  instanceType: t3.small
-  ssh:
-    enableSsm: true
+## Cluster autoscaler
+aws autoscaling \
+  describe-auto-scaling-groups \
+  --query "AutoScalingGroups[? Tags[? (Key=='eks:cluster-name') && Value=='$CLUSTER_NAME']].[AutoScalingGroupName, MinSize, MaxSize,DesiredCapacity]" \
+  --output table
 
-# To enable all of the control plane logs, uncomment below:
-# cloudWatch:
-#  clusterLogging:
-#    enableTypes: ["*"]
 
-EOF
+export ASG_NAME=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[? Tags[? (Key=='eks:cluster-name') && Value=='$CLUSTER_NAME']].AutoScalingGroupName" --output text)
 
-eksctl create cluster -f eksworkshop.yaml
+
+for x in $ASG_NAME; do aws autoscaling update-auto-scaling-group --auto-scaling-group-name  $x  --min-size 3 --desired-capacity 3 --max-size 4; done
+
+eksctl utils associate-iam-oidc-provider \
+    --cluster $CLUSTER_NAME \
+    --approve
+
+aws iam create-policy   \
+  --policy-name k8s-asg-policy \
+  --policy-document file://k8s-asg-policy.json
+
+eksctl create iamserviceaccount \
+  --name cluster-autoscaler \
+  --namespace kube-system \
+  --cluster $CLUSTER_NAME \
+  --attach-policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/k8s-asg-policy" \
+  --approve \
+  --override-existing-serviceaccounts
+
+kubectl -n kube-system describe sa cluster-autoscaler
+
+envsubst < cluster-autoscaler-autodiscover.yaml | kubectl apply -f -
+
+kubectl -n kube-system annotate deployment.apps/cluster-autoscaler  cluster-autoscaler.kubernetes.io/safe-to-evict="false"
+
+# we need to retrieve the latest docker image available for our EKS version
+export K8S_VERSION=$(kubectl version --short | grep 'Server Version:' | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' | cut -d. -f1,2)
+export AUTOSCALER_VERSION=$(curl -s "https://api.github.com/repos/kubernetes/autoscaler/releases" | grep '"tag_name":' | sed -s 's/.*-\([0-9][0-9\.]*\).*/\1/' | grep -m1 ${K8S_VERSION})
+
+kubectl -n kube-system \
+    set image deployment.apps/cluster-autoscaler \
+    cluster-autoscaler=us.gcr.io/k8s-artifacts-prod/autoscaling/cluster-autoscaler:v${AUTOSCALER_VERSION}
