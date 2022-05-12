8eb92ca5f1883f41be065d0db5b1c1adde2bbeb2
Neil Armitage <neil@neila.co.uk>
2022-05-12 08:03:48 +0100

    setup

---
+++ setup/cloudshell.sh
@@ -0,0 +1,8 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo yum -y install jq gettext bash-completion moreutils envsubst
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv -v /tmp/eksctl /usr/local/bin
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
