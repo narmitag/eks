5d1827542920cb7a207e9bb8d9a8a09bf948f59b
Neil Armitage <neil@neila.co.uk>
2022-05-12 09:54:29 +0100

    Update CA

--- cluster-autoscaler/setup-eks.sh
+++ cluster-autoscaler/setup-eks.sh
@@ -1,37 +1,14 @@
-# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#Needs files from https://github.com/narmitag/eks/tree/main/cluster-autoscaler
 
-# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-
-# sudo yum -y install jq gettext bash-completion moreutils envsubst
-
-# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-
-# sudo mv -v /tmp/eksctl /usr/local/bin
-
-
-# eksctl completion bash >> ~/.bash_completion
-# . /etc/profile.d/bash_completion.sh
-# . ~/.bash_completion
 export AWS_REGION=us-east-1
 export CLUSTER_NAME=ca-cluster
 export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-
 
 eksctl create cluster -f ca-cluster.yaml
 
 aws eks update-kubeconfig --name ca-cluster --region=us-east-1
 
 
-
-## Cluster autoscaler
-# aws autoscaling \
-#   describe-auto-scaling-groups \
-#   --query "AutoScalingGroups[? Tags[? (Key=='eks:cluster-name') && Value=='$CLUSTER_NAME']].[AutoScalingGroupName, MinSize, MaxSize,DesiredCapacity]" \
-#   --output table
-
-
-#export ASG_NAME=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[? Tags[? (Key=='eks:cluster-name') && Value=='$CLUSTER_NAME']].AutoScalingGroupName" --output text)
 aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[? Tags[? (Key=='eks:cluster-name') && Value=='$CLUSTER_NAME']].AutoScalingGroupName" --output text > asg.txt
 
 for x in $(cat asg.txt); do aws autoscaling update-auto-scaling-group --auto-scaling-group-name  $x  --min-size 1 --desired-capacity 1 --max-size 2; done
@@ -40,32 +17,27 @@ eksctl utils associate-iam-oidc-provider \
     --cluster $CLUSTER_NAME \
     --approve
 
-#wget https://raw.githubusercontent.com/narmitag/eks/main/cluster-autoscaler/k8s-asg-policy.json
 
 aws iam create-policy   \
-  --policy-name ${CLUSTER}-k8s-asg-policy \
+  --policy-name ${CLUSTER_NAME}-k8s-asg-policy \
   --policy-document file://k8s-asg-policy.json
 
 eksctl create iamserviceaccount \
   --name cluster-autoscaler \
   --namespace kube-system \
   --cluster $CLUSTER_NAME \
-  --attach-policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/${CLUSTER}-k8s-asg-policy" \
+  --attach-policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/${CLUSTER_NAME}-k8s-asg-policy" \
   --approve \
   --override-existing-serviceaccounts
 
 kubectl -n kube-system describe sa cluster-autoscaler
 
-
-#wget https://raw.githubusercontent.com/narmitag/eks/main/cluster-autoscaler/cluster-autoscaler-autodiscover.yaml
 envsubst < cluster-autoscaler-autodiscover.yaml | kubectl apply -f -
 
 kubectl -n kube-system annotate deployment.apps/cluster-autoscaler  cluster-autoscaler.kubernetes.io/safe-to-evict="false"
 
-# we need to retrieve the latest docker image available for our EKS version
-#export K8S_VERSION=$(kubectl version --short | grep 'Server Version:' | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' | cut -d. -f1,2)
-#export AUTOSCALER_VERSION=$(curl -s "https://api.github.com/repos/kubernetes/autoscaler/releases" | grep '"tag_name":' | sed -s 's/.*-\([0-9][0-9\.]*\).*/\1/' | grep -m1 ${K8S_VERSION})
-export AUTOSCALER_VERSION=1.21.2
+
+export AUTOSCALER_VERSION=1.22.2
 kubectl -n kube-system \
     set image deployment.apps/cluster-autoscaler \
     cluster-autoscaler=us.gcr.io/k8s-artifacts-prod/autoscaling/cluster-autoscaler:v${AUTOSCALER_VERSION}
